11c17e3231868df6f1351916e31f63084b277731
k1mh4kj00n <k1mh4kj00n@gmail.com>
2024-11-11 22:35:31 +0900

    feat( Application.java ) Add Business Logic

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,9 +1,19 @@
 package store;
 
+import store.controller.PosMachine;
+import store.view.InputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PosMachine posMachine = PosMachine.getInstance();
+            posMachine.init();
+        do{
+            posMachine.frontLogic();
+            posMachine.productInput();
+            posMachine.receipt();
+            posMachine.reset();
+        }while(InputView.userCheckInput("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"));
     }
-
 
 }
